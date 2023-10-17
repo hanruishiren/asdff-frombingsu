f3d6fa7d1eb6eb414cd24cd0a934a61daf520329
hanruishiren <55995185+hanruishiren@users.noreply.github.com>
2023-10-17 12:44:22 +0800

    改造sd.py使其支持from_single_file加载大模型
    用claude：提供了diffusers的lpw_stable_diffusion_xl.py和loaders.py来学习如何使用loaders.FromSingleFileMixin.from_single_file。提供了asdff的sd.py和base.py供LLM进行代码改造，其中base.py无需改造，只需改造sd.py。

--- asdff/sd.py
+++ asdff/sd.py
@@ -4,15 +4,51 @@ from functools import cached_property
 
 from diffusers import (
     StableDiffusionControlNetInpaintPipeline,
-    StableDiffusionControlNetPipeline,
+    StableDiffusionControlNetPipeline, 
     StableDiffusionInpaintPipeline,
     StableDiffusionPipeline,
 )
 
+#【增加】导入FromSingleFileMixin
+from diffusers.loaders import FromSingleFileMixin
+from diffusers.pipelines.stable_diffusion.convert_from_ckpt import from_ckpt
+
+from pathlib import Path
+
 from asdff.base import AdPipelineBase
 
+#【增加】继承FromSingleFileMixin
+class AdPipeline(AdPipelineBase, StableDiffusionPipeline, FromSingleFileMixin):
 
-class AdPipeline(AdPipelineBase, StableDiffusionPipeline):
+    #【增加】实现from_single_file方法
+    @classmethod
+    def from_single_file(cls, pretrained_model_link_or_path, **kwargs):
+        cache_dir = kwargs.pop("cache_dir", "diffusers_cache")
+        
+        # 删除url前缀
+        for prefix in ["https://huggingface.co/"]:
+            if pretrained_model_link_or_path.startswith(prefix):
+                pretrained_model_link_or_path = pretrained_model_link_or_path[len(prefix):]
+                
+        # 如果提供的是url,下载文件        
+        ckpt_path = Path(pretrained_model_link_or_path)
+        if not ckpt_path.is_file():
+            # 通过hf_hub_download API下载
+            pretrained_model_link_or_path = hf_hub_download(
+                repo_id=ckpt_path.parts[0], 
+                file_path=ckpt_path.parts[1:],
+                cache_dir=cache_dir
+            )
+        
+        # 调用from_ckpt函数加载
+        pipe = from_ckpt(
+            pretrained_model_link_or_path,
+            pipeline_class=cls,
+            **kwargs
+        )
+        
+        return pipe
+
     @cached_property
     def inpaint_pipeline(self):
         return StableDiffusionInpaintPipeline(
@@ -30,8 +66,38 @@ class AdPipeline(AdPipelineBase, StableDiffusionPipeline):
     def txt2img_class(self):
         return StableDiffusionPipeline
 
+#【增加】继承FromSingleFileMixin
+class AdCnPipeline(AdPipelineBase, StableDiffusionControlNetPipeline, FromSingleFileMixin):
 
-class AdCnPipeline(AdPipelineBase, StableDiffusionControlNetPipeline):
+    #【增加】实现from_single_file方法
+    @classmethod
+    def from_single_file(cls, pretrained_model_link_or_path, **kwargs):
+        cache_dir = kwargs.pop("cache_dir", "diffusers_cache")
+        
+        # 删除url前缀
+        for prefix in ["https://huggingface.co/"]:
+            if pretrained_model_link_or_path.startswith(prefix):
+                pretrained_model_link_or_path = pretrained_model_link_or_path[len(prefix):]
+                
+        # 如果提供的是url,下载文件        
+        ckpt_path = Path(pretrained_model_link_or_path)
+        if not ckpt_path.is_file():
+            # 通过hf_hub_download API下载
+            pretrained_model_link_or_path = hf_hub_download(
+                repo_id=ckpt_path.parts[0], 
+                file_path=ckpt_path.parts[1:],
+                cache_dir=cache_dir
+            )
+        
+        # 调用from_ckpt函数加载
+        pipe = from_ckpt(
+            pretrained_model_link_or_path,
+            pipeline_class=cls,
+            **kwargs
+        )
+        
+        return pipe
+
     @cached_property
     def inpaint_pipeline(self):
         return StableDiffusionControlNetInpaintPipeline(
